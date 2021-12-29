966fb35a7c81f4ee251beb073d61298f02ff069a
Stipo Margić <stipo@liberato.io>
2021-12-29 22:03:33 +0100

    Opacity bg

--- src/contact/index.jsx
+++ src/contact/index.jsx
@@ -5,21 +5,22 @@ import "./style.css";
 const Contact = () => {
   return (
     <section className="contact">
+        <div style={{backgroundImage: "url(1.jpg)"}} className="contact-bg" />
       <div className="container">
-        <div className="row">
+          <div className="row">
             <h2 className="text-center py-5">CONTACT US</h2>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 text-center">
-            <div className="icon"><FaUserCircle/> Mijo</div>
-            <div className="icon"><FaPhoneAlt/> + 33333</div>
           </div>
-          <div className="col-lg-6 text-center">
-            <div className="icon"><FaEnvelope/> mail</div>
-            <div className="icon"><FaMapMarker/>adresa</div>
+          <div className="row">
+            <div className="col-lg-6 text-center">
+              <div className="icon"><FaUserCircle/> Mijo</div>
+              <div className="icon"><FaPhoneAlt/> + 33333</div>
+            </div>
+            <div className="col-lg-6 text-center">
+              <div className="icon"><FaEnvelope/> mail</div>
+              <div className="icon"><FaMapMarker/>adresa</div>
+            </div>
           </div>
         </div>
-      </div>
     </section>
   );
 };
